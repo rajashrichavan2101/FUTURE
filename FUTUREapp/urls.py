a80c810ceb99dfe7d02df7247c9c0b19fa7f4ca1
tejalavhad2304 <tejalavhad2304@gmail.com>
2026-02-11 15:14:13 +0530

    Resolved merge conflict

--- FUTUREapp/urls.py
+++ FUTUREapp/urls.py
@@ -2,14 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.mainPage),  
+      
     path('', views.mainPage, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('blog/', views.blog, name='blog'),
     path('places/', views.places_list, name='places_list'),
-    #path('places/create/', views.places_create, name='place_create'),
-    #path('places/update/<int:id>/', views.places_update, name='place_update'),
-    #path('places/delete/<int:id>/', views.places_delete, name='place_delete'),
+
 
 ]
